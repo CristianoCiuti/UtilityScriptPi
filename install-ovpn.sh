3496cbb3558199995d2584642cd3fd84a1967ede
Cristiano Ciuti <cristiano.ciuti@gmail.com>
2022-12-07 17:29:41 +0100

    private conf

--- install-ovpn.sh
+++ install-ovpn.sh
@@ -9,6 +9,7 @@ sudo timedatectl set-timezone Europe/Rome
 # installing Open VPN
 wget https://install.pivpn.io -O pivpn.sh
 wget "https://github.com/CristianoCiuti/UtilityScriptPi/raw/master/conf/pivpn.conf" -O pivpn.conf
+curl -s https://$GITHUB_TOKEN@raw.githubusercontent.com/CristianoCiuti/PrivateConfig/master/pivpn.conf | tee --append pivpn.conf
 chmod +x pivpn.sh && ./pivpn.sh --unattended pivpn.conf
 sudo sed -i '0,/^push "dhcp-option.*/s/^push "dhcp-option.*/push "dhcp-option DNS 192.168.1.1"\n&/' /etc/openvpn/server.conf
 echo 'push "route 192.168.1.0 255.255.255.0"' | sudo tee --append /etc/openvpn/server.conf
